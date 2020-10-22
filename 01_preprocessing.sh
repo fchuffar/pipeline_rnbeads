9e9903be1a4e578698a4807aaeeefa96258a0096
Florent Chuffart <florent.chuffart@gmail.com>
2020-10-22 15:55:07 +0200

    Adding diff ana

--- 01_preprocessing.sh
+++ 01_preprocessing.sh
@@ -1,50 +1,28 @@
-# retrieve fastq
+cd ~/projects/temporise/results/GSE130735
 source config
+pwd
+rsync -auvP ~/projects/${project}/results/${gse}/ cargo:~/projects/${project}/results/${gse}/
 
+# retrieve fastq
 ssh cargo
-
 mkdir -p ~/projects/${datashare}/${gse}/raw
 cd ~/projects/${datashare}/${gse}/raw
-
-for srr in SRR9016929 SRR9016930 SRR9016931 SRR9016934 SRR9016935 SRR9016936
+for srr in SRR3467835 SRR3467836 SRR3467837 SRR3467838 SRR3467839 SRR3467840 SRR3467841 SRR3467842 SRR3467843 
 do
   echo ${srr}
   prefetch $srr
   vdb-validate $srr
   parallel-fastq-dump --threads 16 --tmpdir /dev/shm --gzip --split-files --outdir ./ --sra-id ${srr}
 done
-
 exit 
 
-cd ~/projects/${project}/results/${gse}
+# launch bismark
 rsync -auvP ~/projects/${project}/results/${gse}/ cargo:~/projects/${project}/results/${gse}/
-
 ssh dahu
-
-cd ~/projects/${project}/results/${gse}
 snakemake -s ~/projects/${project}/results/${gse}/wf_rrbs.py --cores 50 --cluster "oarsub --project epimed -l nodes=1/core={threads},walltime=6:00:00 " --latency-wait 60 -pn
 
+# launch RnBeads
+ssh dahu
+oarsub --project epimed  -l /nodes=1,core=32,walltime=06:00:00 "export PATH=/summer/epistorage/miniconda3/bin:/summer/epistorage/opt/bin:\$PATH; cd ~/projects/${project}/results/${gse}; Rscript 02_rnbeads_go.R";
 
-oarsub --project epimed  -l /nodes=1,core=32,walltime=06:00:00 "export PATH=/summer/epistorage/miniconda3/bin:/summer/epistorage/opt/bin:$PATH; cd ~/projects/${project}/results/${gse}; Rscript 02_rnbeads_go.R";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# differential analysis
