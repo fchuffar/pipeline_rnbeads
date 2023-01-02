c2e5c34a8886076124209769b410691ee399fd14
Florent Chuffart <florent.chuffart@gmail.com>
2023-01-02 08:42:48 +0100

    Fixing for GSE

--- 01_preprocessing.sh
+++ 01_preprocessing.sh
@@ -1,12 +1,15 @@
-cd ~/projects/symer/results/rrbs_symer_mgx
+cd ~/projects/dnamaging/vignettes/GSE87648
 source config
 echo ${gse}
 echo ${project}
-rsync -auvP ~/projects/${project}/results/${gse}/ cargo:~/projects/${project}/results/${gse}/
+rsync -auvP ~/projects/${project}/vignettes/${gse}/ cargo:~/projects/${project}/vignettes/${gse}/
+
+
+# for idat from NCBI/GEO, go to 02_rnbeads_gseidat2study
 
 # organize idat
-mkdir -p ~/projects/${datashare}/${gse}/idat
-cd ~/projects/${datashare}/${gse}/idat
+mkdir -p ~/projects/${datashare}/${gse}/raw
+cd ~/projects/${datashare}/${gse}/raw
 ls -lha ~/projects/datashare_epistorage/epic_lfs_diagenode/raw/raw_data/205045*/*.idat | wc
 ln -s ~/projects/datashare_epistorage/epic_lfs_diagenode/raw/raw_data/2050*/*.idat .
 ls -lha
@@ -42,9 +45,9 @@ diff md5sum.bettik.txt md5sum.mgx.txt
 
 
 # launch bismark
-rsync -auvP ~/projects/${project}/results/${gse}/ cargo:~/projects/${project}/results/${gse}/
+rsync -auvP ~/projects/${project}/vignettes/${gse}/ cargo:~/projects/${project}/vignettes/${gse}/
 ssh dahu
-snakemake -s ~/projects/${project}/results/${gse}/wf_rrbs.py --cores 50 --cluster "oarsub --project epimed -l nodes=1/core={threads},walltime=6:00:00 " --latency-wait 60 -pn
+snakemake -s ~/projects/${project}/vignettes/${gse}/wf_rrbs.py --cores 50 --cluster "oarsub --project epimed -l nodes=1/core={threads},walltime=6:00:00 " --latency-wait 60 -pn
 ssh cargo ls projects/${datashare}/${gse}
 mkdir -p ~/projects/${datashare}/${gse}/
 rsync -auvP cargo:projects/${datashare}/${gse}/multiqc_rrbs* ~/projects/${datashare}/${gse}/.
@@ -73,35 +76,35 @@ samtools index /home/fchuffar/projects/datashare/rrbs_symer_mgx/raw/F3C7_S19_R1_
 
 # launch RnBeads
 ssh dahu
-oarsub --project epimed  -l /nodes=1,core=32,walltime=06:00:00 "export PATH=/summer/epistorage/miniconda3/bin:/summer/epistorage/opt/bin:\$PATH; cd ~/projects/${project}/results/${gse}; Rscript 02_rnbeads_go.R";
+oarsub --project epimed  -l /nodes=1,core=32,walltime=06:00:00 "export PATH=/summer/epistorage/miniconda3/bin:/summer/epistorage/opt/bin:\$PATH; cd ~/projects/${project}/vignettes/${gse}; Rscript 02_rnbeads_go.R";
 oarstat -fj ${OAR_JOB_ID}
 tail -f OAR.${OAR_JOB_ID}.stdout 
-echo ${project}/results/${gse}/rnbead_results
-mkdir -p ~/projects/${project}/results/${gse}/rnbead_results/tracks_and_tables_data/sites/trackHub_bigWig/${version}/ 
-rsync -auvP cargo:~/projects/${project}/results/${gse}/rnbead_results/tracks_and_tables_data/sites/trackHub_bigWig/${version}/*.bigWig ~/projects/${project}/results/${gse}/rnbead_results/tracks_and_tables_data/sites/trackHub_bigWig/${version}/.
+echo ${project}/vignettes/${gse}/rnbead_vignettes
+mkdir -p ~/projects/${project}/vignettes/${gse}/rnbead_vignettes/tracks_and_tables_data/sites/trackHub_bigWig/${version}/ 
+rsync -auvP cargo:~/projects/${project}/vignettes/${gse}/rnbead_vignettes/tracks_and_tables_data/sites/trackHub_bigWig/${version}/*.bigWig ~/projects/${project}/vignettes/${gse}/rnbead_vignettes/tracks_and_tables_data/sites/trackHub_bigWig/${version}/.
 
 # run ewas
 ssh dahu
-oarsub --project epimed  -l /nodes=1,core=32,walltime=06:00:00 "export PATH=/summer/epistorage/miniconda3/bin:/summer/epistorage/opt/bin:\$PATH; cd ~/projects/${project}/results/${gse}; Rscript 03_ewas.R";
+oarsub --project epimed  -l /nodes=1,core=32,walltime=06:00:00 "export PATH=/summer/epistorage/miniconda3/bin:/summer/epistorage/opt/bin:\$PATH; cd ~/projects/${project}/vignettes/${gse}; Rscript 03_ewas.R";
 oarstat -fj ${OAR_JOB_ID}
 tail -f OAR.${OAR_JOB_ID}.stdout 
-rsync -auvP cargo:projects/${project}/results/${gse}/ewas_results/ ~/projects/${project}/results/${gse}/ewas_results/
+rsync -auvP cargo:projects/${project}/vignettes/${gse}/ewas_vignettes/ ~/projects/${project}/vignettes/${gse}/ewas_vignettes/
 
 # run combp
 ssh dahu
-oarsub --project epimed  -l /nodes=1,core=32,walltime=06:00:00 "export PATH=/summer/epistorage/miniconda3/bin:/summer/epistorage/opt/bin:\$PATH; cd ~/projects/${project}/results/${gse}; Rscript 03_ewas.R";
-rsync -auvP cargo:projects/${project}/results/${gse}/combp_results/*.regions-p.bed.gz ~/projects/${project}/results/${gse}/combp_results/
+oarsub --project epimed  -l /nodes=1,core=32,walltime=06:00:00 "export PATH=/summer/epistorage/miniconda3/bin:/summer/epistorage/opt/bin:\$PATH; cd ~/projects/${project}/vignettes/${gse}; Rscript 03_ewas.R";
+rsync -auvP cargo:projects/${project}/vignettes/${gse}/combp_vignettes/*.regions-p.bed.gz ~/projects/${project}/vignettes/${gse}/combp_vignettes/
 
 
 
 
 /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin/java -jar /Applications/IGV_2.4.16.app/Contents/Java/igv.jar \
-  ~/projects/${project}/results/${gse}/rnbead_results/tracks_and_tables_data/sites/trackHub_bigWig/mm10/rnbeads_sample_01.bigWig \
-  ~/projects/${project}/results/${gse}/rnbead_results/tracks_and_tables_data/sites/trackHub_bigWig/mm10/rnbeads_sample_02.bigWig \
-  ~/projects/${project}/results/${gse}/rnbead_results/tracks_and_tables_data/sites/trackHub_bigWig/mm10/rnbeads_sample_03.bigWig \
-  ~/projects/${project}/results/${gse}/rnbead_results/tracks_and_tables_data/sites/trackHub_bigWig/mm10/rnbeads_sample_04.bigWig \
-  ~/projects/${project}/results/${gse}/rnbead_results/tracks_and_tables_data/sites/trackHub_bigWig/mm10/rnbeads_sample_05.bigWig \
-  ~/projects/${project}/results/${gse}/rnbead_results/tracks_and_tables_data/sites/trackHub_bigWig/mm10/rnbeads_sample_06.bigWig \
+  ~/projects/${project}/vignettes/${gse}/rnbead_vignettes/tracks_and_tables_data/sites/trackHub_bigWig/mm10/rnbeads_sample_01.bigWig \
+  ~/projects/${project}/vignettes/${gse}/rnbead_vignettes/tracks_and_tables_data/sites/trackHub_bigWig/mm10/rnbeads_sample_02.bigWig \
+  ~/projects/${project}/vignettes/${gse}/rnbead_vignettes/tracks_and_tables_data/sites/trackHub_bigWig/mm10/rnbeads_sample_03.bigWig \
+  ~/projects/${project}/vignettes/${gse}/rnbead_vignettes/tracks_and_tables_data/sites/trackHub_bigWig/mm10/rnbeads_sample_04.bigWig \
+  ~/projects/${project}/vignettes/${gse}/rnbead_vignettes/tracks_and_tables_data/sites/trackHub_bigWig/mm10/rnbeads_sample_05.bigWig \
+  ~/projects/${project}/vignettes/${gse}/rnbead_vignettes/tracks_and_tables_data/sites/trackHub_bigWig/mm10/rnbeads_sample_06.bigWig \
   /Users/florent/projects/${datashare}/${gse}/SRR3467835_1_trimmed_bismark_bt2_sorted.bam \
   /Users/florent/projects/${datashare}/${gse}/SRR3467836_1_trimmed_bismark_bt2_sorted.bam \
   /Users/florent/projects/${datashare}/${gse}/SRR3467837_1_trimmed_bismark_bt2_sorted.bam \
